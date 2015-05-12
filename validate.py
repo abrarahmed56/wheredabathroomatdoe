52cf107cdb94c17605b33e379e52f00e5050c01d
erickolbusz <erickolbusz@gmail.com>
2015-05-12 11:52:33 -0400

    Phone number validation and parsing

--- validate.py
+++ validate.py
@@ -1,7 +1,8 @@
 import string
+import re
 #from jeopardy.final_jeopardy import music as the_bae
-from validate_email import validate_email
-from werkzeug.security import generate_password_hash, check_password_hash
+#from validate_email import validate_email
+#from werkzeug.security import generate_password_hash, check_password_hash
 
 PASSWORD_VALID_CHARS = string.ascii_letters + string.digits + string.punctuation
 
@@ -13,8 +14,18 @@ def is_valid_password(password):
     global PASSWORD_VALID_CHARS
     for char in password:
         if char not in PASSWORD_VALID_CHARS:
-            return False
+            return (False, "Invalid characters in password.")
     return 8 < len(password) < 50
     
+def is_valid_telephone(number):
+    filtered = re.findall(r'\D*([2-9]\d{2})(\D*)([2-9]\d{2})(\D*)(\d{4})\D*', number)
+    if len(filtered) > 1:
+        return (False, "Multiple phone numbers found.")
+    filtered_string = ''.join(filtered[0])
+    for char in filtered_string:
+        if char not in string.digits:
+            filtered_string = filtered_string.replace(char, '')
+    return (True, filtered_string)
+
 def hash_password(s):
     return generate_password_hash(s)
