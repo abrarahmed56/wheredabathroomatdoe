1a7660fcfbdc572cccffd3dc9f11f0575f315678
erickolbusz <erickolbusz@gmail.com>
2015-06-05 11:50:44 -0400

    Restructured email sending, registered email with gmail due to lack of a better service readily available at the time

--- testEmail.py
+++ testEmail.py
@@ -2,22 +2,25 @@ import smtplib
 import socket
 from email.MIMEText import MIMEText
 
-socket.setdefaulttimeout(None)
-HOST = "smtp.gmail.com"
-PORT = "587"
-sender= "erickolbusz@gmail.com"
-password = "#"
-receiver= ""
+def send_email(receiver, subject, body):
+    HOST = "smtp.gmail.com"
+    PORT = "587"
+    socket.setdefaulttimeout(None)
+    sender= "wheredabathroomatdoe@gmail.com"
+    with open('emailpassword', 'r') as f:
+        password = f.read().strip()
+   
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
 
-msg = MIMEText("email body text")
+    server = smtplib.SMTP()
+    server.connect(HOST, PORT)
+    server.starttls()
+    server.login(sender,password)
+    server.sendmail(sender,receiver, msg.as_string())
+    server.close()
 
-msg['Subject'] = 'hey its works bub'
-msg['From'] = sender
-msg['To'] = receiver
-
-server = smtplib.SMTP()
-server.connect(HOST, PORT)
-server.starttls()
-server.login(sender,password)
-server.sendmail(sender,receiver, msg.as_string())
-server.close()
+send_email("erickolbusz@gmail.com","TEST1","hey its works bub")
+send_email("trunkatedpig@gmail.com","TEST2","hey its still works bub")
