f566bd65120d703774d31e7147bb11124229e7fa
ChesleyTan <ChesleyTan97@gmail.com>
2015-06-17 19:55:29 -0400

    Clean up index page and add styling

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,5 @@
 var App = new Marionette.Application();
 
-function initialize() {
-    err = $('flashed_messages');
-    $('select').material_select();
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-            var mapOptions = {
-                zoom: 15,
-                center: myLatlng
-            }
-	})
-    }
-}
-
-
 App.addRegions({
     placeRegion: '#place'
 });
@@ -28,28 +13,22 @@ App.PlaceView = Marionette.ItemView.extend({
     template: "#t",
     tagName: "tr",
     events: {
-	"click #delete": function() {
-	    var h = new Place({type:'bench', address:'bs', rating: 5})
-	    this.model.save(this.model.toJSON());
-	    this.remove();
-	},
-	"click #directions": function() {
-	    var myLatlng;
-	    var locX = this.model.attributes.address[0];
-	    var locY = this.model.attributes.address[1];
-	    var address = [locY, locX];
-	    navigator.geolocation.getCurrentPosition(function(position) {
+        "click #delete": function() {
+            this.remove();
+        },
+        "click #directions": function() {
+            var myLatlng;
+            var locX = this.model.attributes.address[0];
+            var locY = this.model.attributes.address[1];
+            var address = [locY, locX];
+            navigator.geolocation.getCurrentPosition(function(position) {
                 myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-		var replaceURL = "/api/directions/" + address + "/" + myLatlng
-		replaceURL = replaceURL.replace("(", "").replace(")", "").replace(" ", "");
-		window.location.replace(replaceURL);
-	    });
-	}
+                var replaceURL = "/api/directions/" + address + "/" + myLatlng
+                    replaceURL = replaceURL.replace("(", "").replace(")", "").replace(" ", "");
+                window.location.replace(replaceURL);
+            });
+        }
     }
-});
-
-App.PlacesView = Marionette.CollectionView.extend({
-    childView: App.HomeworkView,
 });
 
 App.CompositeView = Marionette.CompositeView.extend({
@@ -62,16 +41,15 @@ var Place = Backbone.Model.extend({
     idAttribute: "PlacesID",
     urlRoot: "/api/marionette",
 });
+
 var Places = Backbone.Collection.extend({
     model: Place,
     url: "/api/marionette",
     initialize: function() {
-	this.fetch();
-	this.on("change: d", function(){console.log('hi');}, this);
+        this.fetch();
     }
 });
 
 var places = new Places();
 
 App.start();
-google.maps.event.addDomListener(window, 'load', initialize);
